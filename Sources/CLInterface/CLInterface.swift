39216bd286ca5d890990f3eaf15c89a74c2f87a1
Nikita Starshinov <nstarshinov@yandex.ru>
2019-07-02 19:41:15 +0300

    Add general documentation

--- Sources/CLInterface/CLInterface.swift
+++ Sources/CLInterface/CLInterface.swift
@@ -1,5 +1,25 @@
 import SPMUtility
 
+/// `CLInterface` protocol should be implemented by a class or a struct that will represent command line interface of your program. Apart from required `description` property, it should contain `@Argument` and `@PositionalArguments` properties for any arguments you want. They will be set automatically after you call `parseArguments`.
+///
+/// - Example:
+///
+/// ```swift
+/// final class Swiftc : CLInteface {
+///     var description = "Swift compiler"
+///
+///     @Argument("--output", "-o", usage: "Write output to <file>")
+///     var outputPath: String?
+///
+///     @Argument("-g", usage: "Emit debug info", default: false)
+///     var debugMode: Bool
+///
+///     @PositionArguments(name: "files", usage: "Files that will be compiled")
+///     var files: [String]
+/// }
+/// ```
+///
+/// After calling `parseArguments`, you will be able to use these properties like normal variables.
 public protocol CLInterface {
     var description: String { get }
     var optionsString: String { get }
--- Sources/TestTool/main.swift
+++ Sources/TestTool/main.swift
@@ -3,14 +3,13 @@ import CLInterface
 
 struct Tool: CLInterface {
     var description = "This custom tool does things"
-    var optionsString = "hello?"
+    var optionsString = "<options>"
     
     @Argument("--output", usage: "Output path") var outputPath: String?
     @Argument("--verbose", "-v", usage: "Verbose output", default: false) var verbose: Bool
 
     @PositionalArguments(name: "files", usage: "files files files")
     var files: [String]
-    
 }
 
 let tool = Tool()

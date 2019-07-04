fc75f3b63c1c379bfa289bb4622fafb1a7ca2430
Nikita Starshinov <nstarshinov@yandex.ru>
2019-07-04 21:55:57 +0300

    Fix issue when default value is provided for optional argument

--- Sources/CLInterface/Argument Types/Argument.swift
+++ Sources/CLInterface/Argument Types/Argument.swift
@@ -12,7 +12,7 @@ public final class Argument<T : ArgumentType> {
             return value as! T
         }
         if let `default` = `default` {
-            return `default`
+            return `default` as! T
         }
         if isOptional {
             return Optional<T.BaseType>.none as! T
@@ -23,7 +23,7 @@ public final class Argument<T : ArgumentType> {
     public let longName: String
     public let shortName: String?
     public let usage: String?
-    public let `default`: T?
+    public let `default`: T.BaseType?
     
     private var isOptional: Bool { T.self == Optional<T.BaseType>.self }
     var missingRequiredArgument: Bool {
@@ -39,7 +39,7 @@ public final class Argument<T : ArgumentType> {
     /// - Parameter shortName: Alternative short name, e.g. "-o"
     /// - Parameter usage: Usage string, describing what this argument does. Shown to user in `--help` menu. If omitted, it won't be shown there
     /// - Parameter default: Optional default value. If provided, it is safe to unwrap this property
-    public init(_ longName: String, _ shortName: String? = nil, usage: String? = nil, default: T? = nil) {
+    public init(_ longName: String, _ shortName: String? = nil, usage: String? = nil, default: T.BaseType? = nil) {
         self.longName = longName
         self.shortName = shortName
         self.usage = usage
